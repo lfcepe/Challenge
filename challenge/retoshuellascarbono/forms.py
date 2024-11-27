56d17cede78b9e6a7f04ad3808a93f154177d96c
felipe <felipecepeda2006@hotmail.com>
2024-11-27 01:07:52 +0000

    modificacion forms

--- challenge/retoshuellascarbono/forms.py
+++ challenge/retoshuellascarbono/forms.py
@@ -1,5 +1,15 @@
 from django import forms
-from models import Retosrespuesta, Dibujos, Poemas
+from models import Retosrespuesta, Dibujos, Poemas, Retos
+
+class Reto_form (forms.ModelForm):
+    class Meta:
+        model: Retos
+        fields: '___all__'
+        widgets: {
+            'reto',
+            'descripcion',
+            'puntos',
+        }
 
 class Retosrespuesta_form (forms.ModelForm):
     class Meta:
--- challenge/retoshuellascarbono/views.py
+++ challenge/retoshuellascarbono/views.py
@@ -3,9 +3,14 @@ from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.views import LoginView
 from django.contrib.auth.decorators import login_required
+from .models import Retos, Retosrespuesta, Poemas, Dibujos, Rankings
+from .forms import Retosrespuesta_form, 
 # Create your views here.
 
 def index(request):
     template = loader.get_template('index.html')
     context = {}
     return HttpResponse(template.render(context, request))
+
+def arte(request):
+    
