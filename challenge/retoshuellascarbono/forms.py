4e30b0eed6ab4468e9bd0630f6836228cffeb04f
felipe <felipecepeda2006@hotmail.com>
2024-11-28 03:23:23 +0000

    registro de usuario

--- challenge/retoshuellascarbono/forms.py
+++ challenge/retoshuellascarbono/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from .models import Retosrespuesta, Dibujos, Poemas, Retos
+from django.contrib.auth.models import User
 
 class Reto_form (forms.ModelForm):
     class Meta:
@@ -36,3 +37,19 @@ class Dibujos_form (forms.ModelForm):
             'nombredibujo', 
             'imagen',
         }
+
+class UserRegistrationForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput)
+    password_confirm = forms.CharField(widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'first_name', 'last_name']
+
+    def clean_password_confirm(self):
+        password = self.cleaned_data.get('password')
+        password_confirm = self.cleaned_data.get('password_confirm')
+        
+        if password != password_confirm:
+            raise forms.ValidationError("Las contraseñas no coinciden.")
+        return password_confirm
--- challenge/retoshuellascarbono/views.py
+++ challenge/retoshuellascarbono/views.py
@@ -3,8 +3,10 @@ from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.views import LoginView
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from django.contrib import messages
 from .models import Retos, Retosrespuesta, Poemas, Dibujos, Rankings
-from .forms import Retosrespuesta_form, Reto_form, Dibujos_form, Poemas_form
+from .forms import Retosrespuesta_form, Reto_form, Dibujos_form, Poemas_form, UserRegistrationForm
 # Create your views here.
 
 def index(request):
@@ -22,3 +24,20 @@ def arte():
 def arte(request):
     poemas = Poemas.objects.order_by('titulopoema')
     dibujos = Dibujos.objects.order_by('')
+
+def registrarusuario(request):
+    if request.method == 'POST':
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            usuario = form.save(commit=False)
+            usuario.set_password(form.cleaned_data['password'])
+            usuario.save()
+            from django.contrib.auth import login
+            login(request, usuario)
+            messages.success(request, "Tu cuenta se ha creado correctamente.")
+            return redirect('login.html') 
+
+    else:
+        form = UserRegistrationForm()
+    
+    return render(request, 'registro.html', {'form': form})
